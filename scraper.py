91f2f0ad65ed2ae8727c1ec52afcda103bdd5d87
Yakshith <yakshith.kommineni@gmail.com>
2024-07-19 17:08:48 -0400

    i got it to retrieve images and save them

--- scraper.py
+++ scraper.py
@@ -10,11 +10,21 @@ soup = BeautifulSoup(r.text, 'html.parser')
 images = soup.find_all('img')
 
 for image in images:
-
-    print(str(image))
-    print('***********')
-    print('')
-    
-'''    with open(name.replace(' ', '-').replace('/', '-') + '.jpg', 'wb') as f:
-        im = requests.get(link)
-        f.write(im.content)'''
+    if 'srcset' in image.attrs:
+        srcset = image.attrs['srcset']
+        urls = srcset.split(',')
+        
+        # Extracting URLs with "xl"
+        for url in urls:
+            if 'xl' in url:
+                # Extracting the URL
+                url = url.split()[0]
+                print(f"Downloading: {url}")
+                img_name = os.path.basename(url.split('?')[0])
+                
+                # Saving the image
+                with open(img_name, 'wb') as f:
+                    img_data = requests.get(url).content
+                    f.write(img_data)
+                print(f"Saved: {img_name}")
+                break  # Assuming you want to save only the first "xl" image per img tag
